ee67247419e9b0ade3f15b7761cf8a067799b3b0
Suein Choi <48196612+sueinchoi@users.noreply.github.com>
2020-11-23 16:52:49 +0900

    Update Analysis.R

--- Modeling/Mouse/Analysis.R
+++ Modeling/Mouse/Analysis.R
@@ -5,9 +5,9 @@ head(dat)
 
 gof1<- dat %>%
     filter(MDV == 0, ROUTE == 2) %>%
-    filter(!(ID %in% c(164, 178, 175, 156, 205, 191, 177, 176, 192, 139))) %>%
-    ggplot(aes(x = DV, y = IPRED)) +
-    geom_point(aes(x = DV, y = IPRED, col = as.factor(ID), shape = as.factor(DOSE))) +
+    filter(!(ID %in% c(164, 178))) %>%
+    ggplot(aes(x = IPRED, y = DV)) +
+    geom_point(aes(x = IPRED, y = DV, col = as.factor(ID), shape = as.factor(DOSE))) +
     stat_smooth(span = 0.7) +
     geom_abline(intercept = 0, slope = 1)
 ggplotly(gof1)
